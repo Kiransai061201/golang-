0e617ea877b8fbc545791ec7fb31c6f243987e71
Kiransai061201 <kiransai061201@gmail.com>
2024-07-15 14:48:36 +0530

    AWS S3 operations using golang

---
+++ AWS_S3_GO/go.mod
@@ -0,0 +1,8 @@
+module mymodule
+
+go 1.22.4
+
+require (
+	github.com/aws/aws-sdk-go v1.54.19 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
---
+++ AWS_S3_GO/uploadfiles.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+)
+
+func UploadFile(uploader *s3manager.Uploader, filePath string, bucketName string, fileName string) error {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(fileName),
+		Body:   file,
+	})
+
+	return err
+}
+
+func main() {
+	sess, err := session.NewSessionWithOptions(session.Options{
+		Profile: "default",
+		Config: aws.Config{
+			Region: aws.String("us-east-1"),
+		},
+	})
+
+	if err != nil {
+		fmt.Printf("Failed to initialize new session: %v", err)
+		return
+	}
+
+	bucketName := "thisbucketcreatedwithgolang"
+	uploader := s3manager.NewUploader(sess)
+	filename := "s3 public access block.png"
+
+	err = UploadFile(uploader, "s3 public access block.png", bucketName, filename)
+	if err != nil {
+		fmt.Printf("Failed to upload file: %v", err)
+		return
+	}
+
+	fmt.Println("Successfully uploaded file!")
+}
