ec90070e3a4254df87a34cc073273c3f26c7e0e6
Kiransai061201 <kiransai061201@gmail.com>
2024-07-17 18:16:40 +0530

    memcached with docker container

--- memcached/memcache.go
+++ memcached/memcache.go
@@ -1,26 +1,33 @@
 package main
 
 import (
-	"fmt"
-	"log"
+    "fmt"
+    "log"
 
-	"github.com/bradfitz/gomemcache/memcache"
+    "github.com/bradfitz/gomemcache/memcache"
 )
 
 func main() {
-	// Initialize a new memcache client
-	mc := memcache.New("localhost:11211")
+    // Initialize a new memcache client
+    mc := memcache.New("localhost:11212")
 
-	// Define the key used for caching
-	key := "facebook_link_cache_key"
+    // Define the key used for caching
+    key := "test_key"
+    value := "Hello, Memcached!"
 
-	// Try to fetch the data from memcache
-	item, err := mc.Get(key)
-	if err != nil {
-		log.Fatalf("Error fetching data from memcache: %v", err)
-	}
+    // Set a value in memcache
+    err := mc.Set(&memcache.Item{Key: key, Value: []byte(value)})
+    if err != nil {
+        log.Fatalf("Error setting value in memcache: %v", err)
+    }
 
-	// Print the cached data
-	fmt.Println("Cached data:")
-	fmt.Println(string(item.Value))
+    // Try to fetch the data from memcache
+    item, err := mc.Get(key)
+    if err != nil {
+        log.Fatalf("Error fetching data from memcache: %v", err)
+    }
+
+    // Print the cached data
+    fmt.Println("Cached data:")
+    fmt.Println(string(item.Value))
 }
